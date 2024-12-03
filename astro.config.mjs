2b7b07346657bc66ffba886b90d7159aad053c1c
jenpennen <jennycho35@gmail.com>
2024-12-03 01:48:08 -0800

    add site in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  site: "https://jenpennen.github.io/jennyecho.github.io",
 });
